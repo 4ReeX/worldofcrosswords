0330df0d59ce50656b0a44dc8eb88da397d38a34
Marazmen <jetracoon@gmail.com>
2022-10-21 14:16:42 +0300

    mir crosswords +1

--- js/scripts.js
+++ js/scripts.js
@@ -37,11 +37,8 @@ function loadRules() {
 }
 function nextRules() {
     $('.popup').toggleClass("overflow-v");
-    $('.popup-content').load('rules2.html');
-    $('.close').addClass('hide');
-}
-function nextBonus() {
     $('.popup-content').load('bonus.html');
+    $('.close').addClass('hide');
 }
 function loadPuzzle() {
     $('.popup, .shadow').toggleClass("hide");
@@ -75,13 +72,88 @@ function buyLink() {
     $('.popup, .shadow').addClass("hide");
     window.location.href = "store.html";
 }
-function buyLink() {
-    $('.popup, .shadow').toggleClass("hide");
-    $('.popup-content').load('buy.html');
-}
-
 
 function couponOpen() {
     $('.popup, .shadow').toggleClass("hide");
     $('.popup-content').load('couponDescription.html');
+}
+function rulesSudoku() {
+    $('.rules-pop').toggleClass("hide");
+    $('.rules-pop').load('rules-sudoku.html');
+}
+function rulesJapan() {
+    $('.rules-pop').toggleClass("hide");
+    $('.rules-pop').load('rules-japan.html');
+}
+function rulesFillWords() {
+    $('.rules-pop').toggleClass("hide");
+    $('.rules-pop').load('rules-fillwords.html');
+}
+
+
+setTimeout(function closeFooter() {
+    $(".footer").toggleClass("closed");
+    $(".menu-toggle").toggleClass("closed");
+    $(".header").toggleClass("closed");
+}, 5000)
+
+$(".menu-toggle").click(function () {
+    $(".menu-toggle").toggleClass("closed");
+    $(".footer").toggleClass("closed");
+    $(".header").toggleClass("closed");
+    setTimeout(function closeFooter() {
+        $(".footer").toggleClass("closed");
+        $(".menu-toggle").toggleClass("closed");
+        $(".header").toggleClass("closed");
+    }, 5000)
+});
+
+
+function loadJWin() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('jwin.html');
+}
+function loadJLose() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('jlose.html');
+}
+
+function loadSWin() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('swin.html');
+}
+function loadSLose() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('slose.html');
+}
+
+function loadFWin() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('fwin.html');
+}
+function loadFLose() {
+    $('.popup, .shadow').toggleClass("hide");
+    $('.popup').load('flose.html');
+}
+
+
+
+function setZoom(zoom,el) {
+    transformOrigin = [0,0];
+    el = el || instance.getContainer();
+    var p = ["webkit", "moz", "ms", "o"],
+        s = "scale(" + zoom + ")",
+        oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+
+    for (var i = 0; i < p.length; i++) {
+        el.style[p[i] + "Transform"] = s;
+        el.style[p[i] + "TransformOrigin"] = oString;
+    }
+    el.style["transform"] = s;
+    el.style["transformOrigin"] = oString;
+}
+//setZoom(5,document.getElementsByClassName('container')[0]);
+function showVal(a){
+    var zoomScale = Number(a)/10;
+    setZoom(zoomScale,document.getElementsByClassName('container')[0])
 }
